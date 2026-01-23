d81a67b623baf00aacca49205139535d19dcfd35
Yifan Hao <yifanhao5@gmail.com>
2026-01-22 16:42:31 -0800

    temp remove exec members component

--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
       <HeroSection />
       <Events />
       <AboutSection />
-      <ExecMembers />
+      {/* <ExecMembers /> */}
       <FAQ />
       <Footer />
     </main>
